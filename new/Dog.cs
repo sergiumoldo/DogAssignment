1621b8f44ec1d935c6167c14a5025dcbff318226
Moldovan Sergiu <Moldovan Sergiu@LAPTOP-GUL0Q4DJ>
2023-08-03 21:00:47 +0300

    Add project files.

---
+++ new/Dog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Dog
+{
+    private int age;
+    private string name;
+    public Dog(string name, int age)
+    {
+       Name = name;
+       Age = age;
+    }
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            if(value.Length <= 2)
+            {
+                throw new ApplicationException("Name must be longer than 2 characters");
+            }
+            this.name = value;
+
+        }
+    }
+    public int Age 
+    { 
+        get { return this.age; }
+        set
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Age must be higher than 0 ");
+            }
+
+            if (value > 100)
+            {
+                throw new ApplicationException("Age must be lower than 100 ");
+            }
+
+            this.age = value;
+
+        } 
+    }
+}
+
+
---
+++ new/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Linq;
+
+//Having the class Dog with Age and Name properties, add the next changes:
+
+//-throw ArgumentException for negative age
+
+//-throw ApplicationException for age greater than 100
+
+//- throw ApplicationException for name length lower than 2
+
+//Then create a dog object in Main method using object initializer to set up the age and name. Watch for expected exceptions and send a friendly message to the user with the exception.
+
+try
+{
+    Console.WriteLine("Insert dog's name:");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Insert dog's age:");
+    int age = int.Parse(Console.ReadLine());
+
+    Dog dog = new Dog(name, age);
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (ApplicationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Thank you for playing !");
+}
+
+
+
+
+
